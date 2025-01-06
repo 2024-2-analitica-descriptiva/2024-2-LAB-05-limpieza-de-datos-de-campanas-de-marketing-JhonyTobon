4178585d730be5e4db3278da390f2ed23c674d34
Jhony Andrés Tobón Hoyos <jtobonh@unal.edu.co>
2025-01-05 22:38:09 -0500

    Limpieza_01

--- homework/homework.py
+++ homework/homework.py
@@ -4,12 +4,20 @@ Escriba el codigo que ejecute la accion solicitada.
 
 # pylint: disable=import-outside-toplevel
 
+import zipfile
+import pandas as pd
+import os
+
+input_dir = 'files/input'
+output_dir = 'files/output'
+
+os.makedirs(output_dir, exist_ok=True)
 
 def clean_campaign_data():
     """
     En esta tarea se le pide que limpie los datos de una campaña de
     marketing realizada por un banco, la cual tiene como fin la
-    recolección de datos de clientes para ofrecerls un préstamo.
+    recolección de datos de clientes para ofrecerles un préstamo.
 
     La información recolectada se encuentra en la carpeta
     files/input/ en varios archivos csv.zip comprimidos para ahorrar
@@ -36,7 +44,7 @@ def clean_campaign_data():
     - number_contacts
     - contact_duration
     - previous_campaing_contacts
-    - previous_outcome: cmabiar "success" por 1, y cualquier otro valor a 0
+    - previous_outcome: cambiar "success" por 1, y cualquier otro valor a 0
     - campaign_outcome: cambiar "yes" por 1 y cualquier otro valor a 0
     - last_contact_day: crear un valor con el formato "YYYY-MM-DD",
         combinando los campos "day" y "month" con el año 2022.
@@ -49,9 +57,47 @@ def clean_campaign_data():
 
 
     """
+    df_all= pd.DataFrame()
 
-    return
+    for filename in os.listdir(input_dir):
 
+        if filename.endswith('.zip'):
+
+            zip_path = os.path.join(input_dir,filename)
+
+
+            with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+
+                csv_file = zip_ref.namelist()[0]
+
+                    
+                with zip_ref.open(csv_file) as my_file:
+
+                    df_temp = pd.read_csv(my_file)
+                    df_all = pd.concat([df_all,df_temp], ignore_index=True)
+
+    return df_all
+
+df_client = clean_campaign_data()[['client_id','age','job','marital','education','credit_default', 'mortgage'].copy()]
+df_client['job'] = df_client['job'].str.replace('.','').str.replace('-','_')
+df_client['education'] = df_client['education'].replace('unknow',pd.NA).str.replace('.','_')
+df_client['credit_default'] = df_client ['credit_default'].map({'yes':'1', 'no':'0'})
+df_client['mortgage'] = df_client['mortgage'].map({'yes':'1','no':'2'})
+
+df_campaign = clean_campaign_data()[['client_id','number_contacts','contact_duration','previous_campaign_contacts','previous_outcome','campaign_outcome','day', 'month'].copy()]
+
+df_campaign['previous_outcome'] = df_campaign['previous_outcome'].replace('success',1)
+df_campaign['previous_outcome'] = df_campaign['previous_outcome'].apply(lambda x: 0 if x != 1 else x)
+df_campaign['campaign_outcome'] = df_campaign['campaign_outcome'].replace('yes',1)
+df_campaign['campaign_outcome'] = df_campaign['campaign_outcome'].apply(lambda x: 0 if x != 1 else x)
+df_campaign['last_contact_date'] = pd.to_datetime('2022 '+ df_campaign['month']+' '+ df_campaign['day'].astype(str))
+df_campaign['last_contact_date'] = df_campaign['last_contact_date'].dt.strftime('%Y-%m-%d')
+df_campaign = df_campaign [['client_id','number_contacts','contact_duration','previous_campaign_contacts','previous_outcome','campaign_outcome','last_contact_date'].copy()]
+df_economics = clean_campaign_data()[['client_id', 'cons_price_idx', 'euribor_three_months']].copy()
+
+df_client.to_csv('files/output/client.csv', index=False)
+df_campaign.to_csv('files/output/campaign.csv', index=False)
+df_economics.to_csv('files/output/economics.csv', index=False)
 
 if __name__ == "__main__":
     clean_campaign_data()
